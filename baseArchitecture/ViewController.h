3138919f08a0576c4d868376457eade339616fac
wangfei02 <wangfei02@chinatopcredit.com>
2016-07-13 10:17:23 +0800

    Initial Commit

---
+++ baseArchitecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baseArchitecture
+//
+//  Created by Mackellen on 16/7/13.
+//  Copyright © 2016年 Mackellen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
